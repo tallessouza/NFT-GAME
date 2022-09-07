cfd93512d99a5c2e3ccb594ed2f47306ddfc8d4c
tallessouza <talles-swd@live.com>
2022-09-07 13:37:39 -0300

    NFT GAME

---
+++ gameapp/src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x6c494A633F89f3C2C076BB131F11976cE532aE5d';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
---
+++ scripts/deploy.js
@@ -0,0 +1,32 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+  const gameContract = await gameContractFactory.deploy(
+    ["Homem Mermaid", "Little Mexilhao", "The Bob"],
+    [
+      "https://gateway.pinata.cloud/ipfs/QmacMyrYuzi9TwugQk7X9LMfcphVSrtBd46AwAHWS7va8g",
+      "https://gateway.pinata.cloud/ipfs/QmTji3uRPvV3TDcggeQUpWff2nXkQsMgCwAkptGxDiidPb",
+      "https://gateway.pinata.cloud/ipfs/QmZ5qbv8pVXffovbtb3wZawxnhZv3W2aTxkZxkjX1F6EsF",
+    ],
+    [100, 200, 300],
+    [100, 50, 25],
+    "Plank",
+    "https://gateway.pinata.cloud/ipfs/QmT6YHgRZcSvmR5z7kGYc6FPGzf1cj3u4cyuACrfdkWXo4",
+    10000,
+    50
+  );
+  await gameContract.deployed();
+  console.log("Contrato implantado no endereço:", gameContract.address);
+
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
